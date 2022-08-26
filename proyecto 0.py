cd05defb6e2a7c06e189f998c9a99a17d04b55c2
Juanvalencia112 <j.valenciam>
2022-08-25 19:13:38 -0500

    first commit

---
+++ proyecto 0.py
@@ -0,0 +1,24 @@
+import ply.lex as lex
+import re
+import codecs
+import os
+import sys
+
+
+tokens = ['ASSIGN', 'WALKn', 'JUMP', 'JUMPTO', 'VEER' 
+        , 'LOOK', 'DROP', 'GRAB', 'GET', 'FREE', 'POP'
+        , 'WALKdn', 'WALKon', 'M','R' , 'C', 'B'
+        , 'c', 'b', 'P', 'JN', 'Gxy']
+
+
+reservadas = {'ifelse':'IFELSE'
+        ,'if':'IF', 'while':'WHILE', 'repeat':'REPEAT'
+        ,'prog':'PROG', 'gorp':'GORP', 'var':'VAR'
+        , 'proc':'PROC', 'corp':'CORP' } 
+
+
+
+tokens = tokens + list(reservadas.values())
+
+t_ASSIGN = r':='
+
